e27362aa7a469b228d4a1baa5625880f7bdad530
任家乐 <renjiale@yuewen.com>
2020-03-25 19:41:17 +0800

    add babel in js task

--- tasks/js.js
+++ tasks/js.js
@@ -21,7 +21,9 @@ var lbfTransport = require('./plugins/gulp-lbf-transport/');
 var src = path.join(PROJECT_ABS_PATH, TASK_CONFIG.src);
 var dest = path.join(PROJECT_ABS_PATH, TASK_CONFIG.dest);
 var sourceMapDes = path.join(PROJECT_ABS_PATH, TASK_CONFIG.sourcemap);
+var babel = require('gulp-babel');
 
+console.log("js dest dir:", dest);
 // JS代码检查
 gulp.task('js:eslint', function() {
     return gulp.src(path.join(src, '**/*.js'))
@@ -34,7 +36,7 @@ gulp.task('js:eslint', function() {
 
 // JS模块转换
 gulp.task('js:transport', function() {
-    return gulp.src(path.join(src, '**/*.js'))
+    return gulp.src(path.join(dest, '**/*.js'))
         .pipe(plugins.plumber())
         .pipe(plugins.changed(dest))
         .pipe(gulp.dest(dest))
@@ -60,7 +62,7 @@ gulp.task('js:eslint:build', function() {
 
 // JS模块转换
 gulp.task('js:transport:build', function() {
-    return gulp.src(path.join(src, '**/*.js'))
+    return gulp.src(path.join(dest, '**/*.js'))
         .pipe(plugins.plumber())
         .pipe(gulp.dest(dest))
         .pipe(lbfTransport(TASK_CONFIG.lbfTransport))
@@ -75,15 +77,23 @@ gulp.task('js:copy:build', function () {
 
 
 gulp.task('js', function (done) {
-        gulp.watch(path.join(src, '**/*.js'), ['js:eslint', 'js:transport','js:copy'])
+        gulp.watch(path.join(src, '**/*.js'), ['js:eslint', 'js:babel', 'js:transport', 'js:copy'])
         .on('change',function(event){
             console.log(chalk.green('[文件变化:JS]' + event.path));
         })
-    runSequence('js:eslint', 'js:transport','js:copy', done);
+    runSequence('js:eslint', 'js:babel', 'js:transport', 'js:copy', done);
 });
 
 gulp.task('js:build', function (done) {
-    runSequence('js:eslint:build', 'js:transport:build','js:copy:build', done);
+    runSequence('js:eslint:build', 'js:babel', 'js:transport:build', 'js:copy:build', done);
 });
 
-
+// 添加babel任务
+gulp.task('js:babel', () =>
+    gulp.src(path.join(src, '**/*.js'))
+        .pipe(plugins.plumber())
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(gulp.dest(dest))
+);
